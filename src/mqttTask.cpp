a2cb1fb7a281cea5a3f8ccea874e79e78dde0671
Ethan <ethanhharris5@gmail.com>
2023-12-24 13:28:23 +1100

    Added MQTT Setup Function

--- src/mqttTask.cpp
+++ src/mqttTask.cpp
@@ -8,3 +8,15 @@ int BROKER_PORT;
 const char *DEVICE_NAME;
 const char *RX_TOPIC;
 
+void setup_mqtt(const char *MQTT_BROKER_IP, const int MQTT_BROKER_PORT, const char *DEV_NAME, const char *MANAGEMENT_TOPIC) {
+  BROKER_IP = MQTT_BROKER_IP;
+  BROKER_PORT = MQTT_BROKER_PORT;
+  DEVICE_NAME = DEV_NAME;
+  RX_TOPIC = MANAGEMENT_TOPIC;
+
+  client.setServer(BROKER_IP, BROKER_PORT);
+
+  client.setCallback(message_rx_callback);
+
+  return;
+}
